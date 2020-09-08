406ae521791b970b44f091a8a347e037bb25acf7
alexchung <yonganzhong@outlook.com>
2020-09-08 16:37:15 +0800

    update train module

--- train.py
+++ train.py
@@ -23,23 +23,33 @@ from data.dataset_pipeline import dataset_batch
 from libs.nets.model import LSTM
 
 
-
-
 def main(argv):
 
     # -------------------load dataset-------------------------------------------
     (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=cfgs.FEATURE_SIZE)
 
-    # turn the samples into 2D tensor of shape (num_samples, max_length)
-    x_train = preprocessing.sequence.pad_sequences(x_train, cfgs.MAX_LENGTH)
-    y_train = np.asarray(y_train).astype(np.float32)
-    x_test = preprocessing.sequence.pad_sequences(x_test, cfgs.MAX_LENGTH)
-    y_train = np.asarray(y_train).astype(np.float32)
-    num_train_samples = len(x_train)
-    num_test_samples = len(x_test)
+    # use train dataset to train and validation model
+    x_dataset = x_train
+    y_dataset = y_train
 
-    train_dataset = dataset_batch(x_train, y_train, batch_size=cfgs.BATCH_SIZE, is_training=True)
-    test_dataset =  dataset_batch(x_test, y_test, batch_size=cfgs.BATCH_SIZE, is_training=False)
+    # turn the samples into 2D tensor of shape (num_samples, max_length)
+    x_dataset = preprocessing.sequence.pad_sequences(x_dataset, cfgs.MAX_LENGTH)
+    y_dataset = np.asarray(y_dataset).astype(np.float32)
+    # x_test = preprocessing.sequence.pad_sequences(x_test, cfgs.MAX_LENGTH)
+    # y_train = np.asarray(y_train).astype(np.float32)
+
+    num_val_samples = int(np.floor(len(x_dataset) * cfgs.SPLIT_RATIO))
+    num_train_samples = len(x_dataset) - num_val_samples
+
+    # shuffle dataset
+    indices = np.arange(len(x_train))
+    np.random.shuffle(indices)
+    x_dataset = x_dataset[indices]
+    y_dataset = y_dataset[indices]
+
+    # split dataset
+    x_train, y_train = x_dataset[:num_train_samples], y_dataset[:num_train_samples]
+    x_val, y_val = x_dataset[num_train_samples:], y_dataset[num_train_samples:]
 
     # --------------------- construct model------------------------------------------
     model = LSTM(input_length=cfgs.MAX_LENGTH, feature_size=cfgs.FEATURE_SIZE, embedding_size= cfgs.EMBEDDING_SIZE,
@@ -75,16 +85,20 @@ def main(argv):
         summary_op = tf.summary.merge_all()
 
         train_step_per_epoch = num_train_samples // cfgs.BATCH_SIZE
-        test_step_pre_epoch = num_test_samples // cfgs.BATCH_SIZE
+        test_step_pre_epoch = num_val_samples // cfgs.BATCH_SIZE
 
+        # generate batch
+        train_dataset = dataset_batch(x_train, y_train, batch_size=cfgs.BATCH_SIZE, is_training=True)
+        val_dataset = dataset_batch(x_val, y_val, batch_size=cfgs.BATCH_SIZE, is_training=False)
         train_data_batch, train_label_batch = train_dataset.get_next()
-        test_data_batch, test_label_batch = test_dataset.get_next()
-        for epoch in range(1, cfgs.NUM_EPOCH+1):
+        val_data_batch, val_label_batch = val_dataset.get_next()
+        # use k folder validation
+        for epoch in range(cfgs.NUM_EPOCH):
             train_bar = tqdm(range(1, train_step_per_epoch+1))
             for step in train_bar:
                 x_train, y_train = sess.run([train_data_batch, train_label_batch])
                 y_train = y_train[:, np.newaxis]
-                feed_dict = model.fill_feed_dict(x_train, y_train, keep_prob=cfgs.KEPP_PROB)
+                feed_dict = model.fill_feed_dict(x_train, y_train, keep_prob=cfgs.KEEP_PROB)
                 summary, global_step, train_loss, train_acc, _ = sess.run([summary_op, model.global_step, model.loss, model.acc, model.train],
                                                                           feed_dict=feed_dict)
                 if step % cfgs.SMRY_ITER == 0:
@@ -96,7 +110,7 @@ def main(argv):
             test_loss_list = []
             test_acc_list = []
             for step in range(test_step_pre_epoch):
-                x_test, y_test = sess.run([test_data_batch, test_label_batch])
+                x_test, y_test = sess.run([val_data_batch, val_label_batch])
                 y_test = y_test[:, np.newaxis]
                 feed_dict = model.fill_feed_dict(x_test, y_test, keep_prob=1.0)
 
